0576b71bdd885224226e64a882e2af17456d9063
Seth Samuel <sethfsamuel@gmail.com>
2022-12-05 10:00:48 -0500

    Day 3 part 2

--- src/day3/mod.rs
+++ src/day3/mod.rs
@@ -28,6 +28,31 @@ fn find_overlaps(rucksacks: &Vec<Rucksack>) -> Vec<char> {
     rucksacks.iter().map(|r| find_overlap(r)).collect()
 }
 
+fn find_elf_overlap(rucksacks: (&Rucksack, &Rucksack, &Rucksack)) -> char {
+    let pack1 = [rucksacks.0 .0.clone(), rucksacks.0 .1.clone()].concat();
+
+    let pack2 = [rucksacks.1 .0.clone(), rucksacks.1 .1.clone()].concat();
+    let pack3 = [rucksacks.2 .0.clone(), rucksacks.2 .1.clone()].concat();
+    let intersect1 = pack1
+        .iter()
+        .collect::<HashSet<_>>()
+        .intersection(&pack2.iter().collect::<HashSet<_>>())
+        .map(|c| *c)
+        .collect::<HashSet<_>>();
+
+    let pack3_set = pack3.iter().collect::<HashSet<_>>();
+    let intersect = intersect1.intersection(&pack3_set);
+
+    ***intersect.collect::<Vec<_>>().first().unwrap()
+}
+
+fn find_elf_overlaps(rucksacks: &Vec<Rucksack>) -> Vec<char> {
+    rucksacks
+        .chunks(3)
+        .map(|chunk| find_elf_overlap((&chunk[0], &chunk[1], &chunk[2])))
+        .collect()
+}
+
 fn sum_overlaps(overlaps: &Vec<char>) -> u32 {
     let values = overlaps.iter().map(|c| {
         if c.is_uppercase() {
@@ -70,6 +95,20 @@ CrZsJsPPZsGzwwsLwLmpwMDw";
         let overlaps: Vec<char> = find_overlaps(&rucksacks);
         assert_eq!(sum_overlaps(&overlaps), 157);
     }
+
+    #[test]
+    fn test_find_elf_overlap() {
+        let rucksacks = parse_file(TEST_STR);
+        let overlap: char = find_elf_overlap((&rucksacks[0], &rucksacks[1], &rucksacks[2]));
+        assert_eq!(overlap, 'r');
+    }
+
+    #[test]
+    fn test_sum_elf_overlaps() {
+        let rucksacks = parse_file(TEST_STR);
+        let overlaps: Vec<char> = find_elf_overlaps(&rucksacks);
+        assert_eq!(sum_overlaps(&overlaps), 70);
+    }
 }
 
 #[allow(dead_code)]
@@ -83,7 +122,9 @@ pub fn part1() {
 
 #[allow(dead_code)]
 pub fn part2() {
-    // let contents = super::read_file(module_path!());
+    let contents = super::read_file(module_path!());
+    let rucksacks = parse_file(&contents);
+    let overlaps: Vec<char> = find_elf_overlaps(&rucksacks);
 
-    // println!("{}", score_rounds(parse_file_2(contents.as_str())))
+    println!("{}", sum_overlaps(&overlaps));
 }
