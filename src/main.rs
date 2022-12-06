2e25908bd79aece27d23f9752ff5a12e9340e2bc
Seth Samuel <sethfsamuel@gmail.com>
2022-12-05 23:25:03 -0500

    WIP

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,9 @@ mod day1;
 mod day2;
 mod day3;
 mod day4;
+mod day5;
 mod file;
 
 fn main() {
-    // day1::part1();
-    // day1::part2();
-    // day2::part1();
-    // day2::part2();
-    // day3::part1();
-    // day3::part2();
-    // day4::part1();
-    day4::part2();
+    day5::part1();
 }
